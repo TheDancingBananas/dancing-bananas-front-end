a21d80be1620b8ae095472290b27167a89421184
Jason Jacobs <nerfnerd@gmail.com>
2021-07-29 16:16:40 -0700

    update the bot to not keep repeating messages and use new intro message

--- packages/bots/banana-telegram-alerts.js
+++ packages/bots/banana-telegram-alerts.js
@@ -23,8 +23,11 @@ bot.on('message', (msg) => {
     let user = get_user(msg.chat.id);
 
     if(user == -1) {
-        bot.sendMessage(msg.chat.id,"Hi I will let you know when you have bananas ready. Could you send me your hash so I can let you know when you have bananas ready! 🍌 🍌 🍌");
-        //
+        bot.sendMessage(msg.chat.id,"🍌I'm the Dancing Banana Monkey Bot!🍌");
+        bot.sendMessage(msg.chat.id,"🙊🙊🙊In X hours, I will let you know when you have bananas ready! 🙊🙊🙊");
+
+        setTimeout(function() {bot.sendMessage(msg.chat.id, "🍌🍌🍌🍌 You have new bananas ready! 🍌🍌🍌🍌"); }, 20000);
+
         let new_user = {
             id: msg.chat.id,
             hash: ''
@@ -32,16 +35,7 @@ bot.on('message', (msg) => {
 
         users.push(new_user);
     } else {
-
-        if (user.hash === "") {
-            user.hash = msg.text.toString().toLowerCase();
-            bot.sendMessage(msg.chat.id,"Thanks! We will check for when you have new bananas ready!");
-            setInterval(function() {bot.sendMessage(msg.chat.id, "🍌🍌🍌🍌 You have new bananas ready! 🍌🍌🍌🍌"); }, 20000);
-        } else {
             bot.sendMessage(msg.chat.id,"Please wait your bananas are not ready yet!");
-        }
-
-
     }
 });
 
