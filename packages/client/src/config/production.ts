f1823b696f0a274555eb8565bbba9f29f4d1692d
Jason Jacobs <nerfnerd@gmail.com>
2021-08-13 17:15:39 -0700

    Change production.ts for future.

--- packages/client/src/config/production.ts
+++ packages/client/src/config/production.ts
@@ -1,7 +1,7 @@
 import AppConfig from 'types/app-config';
 
 const config: Partial<AppConfig> = {
-    wsApi: 'wss://app.dancingbananas.fun/realtime',
+    wsApi: 'wss://dancingbananas.fun/realtime',
 };
 
 export default config;
