e6af5734c2d257c7125a554a0996eeb367e20338
wdev733 <wdev733@gmail.com>
2021-08-26 11:39:57 -0700

    fix level2 rewards, shop page

--- packages/client/src/containers/tabs/shop-container.tsx
+++ packages/client/src/containers/tabs/shop-container.tsx
@@ -3,15 +3,23 @@ import { useState, useContext, useEffect, useReducer } from 'react';
 
 import classNames from 'classnames';
 
+import { storage } from 'util/localStorage';
+import { Level, LevelTask, RewardItem, Rewards } from 'types/game';
+import gameData from 'constants/gameData.json';
+
 import './shop-container.scss';
 import pngRewardRoof from 'styles/images/reward-roof.png';
 import pngDancingBanana from 'styles/images/dancing-banana.png';
 
+import png3Hours from 'styles/images/3hours.png';
+import png3HoursGray from 'styles/images/3hours-gray.png';
+import png2Hours from 'styles/images/2hours.png';
+import png2HoursGray from 'styles/images/2hours-gray.png';
+
 import pngSpeedUp from 'styles/images/shop/Monkey_rocket.png';
-import pngRemoveBanana from 'styles/images/banana-3.png';
+import pngRemoveBanana from 'styles/images/banana-4.png';
 import pngMonkeyHappy from 'styles/images/monkey-1.png';
 import pngMonkeys from 'styles/images/monkeys.png';
-import png3Hours from 'styles/images/3hours.png';
 import pngSpeed from 'styles/images/speed.png';
 import pngLock from 'styles/images/lock.png';
 import pngArrowLeft from 'styles/images/left.png';
@@ -27,7 +35,7 @@ const ShopItem = ({
     onExchange,
 }: {
     itemName: string | JSX.Element;
-    itemImage: string;
+    itemImage?: string;
     banana: number;
     active?: boolean | false;
     imageHeight?: number;
@@ -35,6 +43,7 @@ const ShopItem = ({
     onExchange: () => void;
 }): JSX.Element | null => {
     const style = imageHeight ? { height: `${imageHeight}px` } : {};
+    const fontStyle = itemImage ? {} : { fontSize: 20, lineHeight: '24px' };
     const isSpeedUp = itemName === 'SPEED UP 48';
 
     const handleExchange = () => {
@@ -42,6 +51,7 @@ const ShopItem = ({
             onExchange();
         }
     };
+
     return (
         <div className='reward-item-wrapper'>
             <div
@@ -52,12 +62,14 @@ const ShopItem = ({
                 )}
                 onClick={handleExchange}
             >
-                <img src={itemImage} style={style} />
-                <span className='reward-item-name'>{itemName}</span>
+                {itemImage && <img src={itemImage} style={style} />}
+                <span className='reward-item-name' style={fontStyle}>
+                    {itemName}
+                </span>
                 {isSpeedUp && (
                     <span className='reward-item-subtitle'>48 HRS</span>
                 )}
-                {active && (
+                {active && banana > 0 && (
                     <div className='reward-item-button' role='button'>
                         <span>{banana}</span>
                         {` `}
@@ -81,6 +93,22 @@ const ShopContainer = ({
         console.log('unlocked clicked');
     };
 
+    const currentLevel = storage.getLevel();
+
+    const gameLevels: Level[] = gameData.game;
+
+    const rewards: Rewards[] = ['SPEED UP 48']; // default for level 1
+
+    for (let i = 0; i < Number(currentLevel); i++) {
+        const levelRewards = gameLevels[i].rewards;
+        for (let j = 0; j < levelRewards.length; j++) {
+            if (!rewards.includes(levelRewards[j] as Rewards)) {
+                rewards.push(levelRewards[j] as Rewards);
+            }
+        }
+    }
+
+    console.log(rewards);
     return (
         <div className='reward-container'>
             <img className='reward-container-image' src={pngRewardRoof} />
@@ -96,7 +124,7 @@ const ShopContainer = ({
                         itemName='SPEED UP 48'
                         itemImage={pngSpeedUp}
                         banana={300}
-                        active={true}
+                        active={rewards.includes('SPEED UP 48')}
                         imageHeight={140}
                         onUnlock={() => handleUnlock()}
                         onExchange={() => onExchange('speedup')}
@@ -110,34 +138,22 @@ const ShopContainer = ({
                             </>
                         }
                         itemImage={pngRemoveBanana}
-                        banana={1}
+                        banana={0}
+                        active={rewards.includes('REMOVE LIQUIDITY')}
                         onUnlock={() => handleUnlock()}
                         onExchange={() => onExchange('')}
                     />
                     <ShopItem
                         itemName={
                             <>
-                                BANANA
-                                <br />
-                                PORTFOLIO
-                            </>
-                        }
-                        itemImage={pngMonkeyHappy}
-                        banana={1}
-                        onUnlock={() => handleUnlock()}
-                        onExchange={() => onExchange('')}
-                    />
-                    <ShopItem
-                        itemName={
-                            <>
-                                SENTIMENT
+                                EMOTION
                                 <br />
                                 PRICE RANGES
                             </>
                         }
                         itemImage={pngMonkeys}
-                        banana={1}
                         imageHeight={50}
+                        banana={1}
                         onUnlock={() => handleUnlock()}
                         onExchange={() => onExchange('')}
                     />
@@ -149,7 +165,7 @@ const ShopContainer = ({
                                 WAIT
                             </>
                         }
-                        itemImage={png3Hours}
+                        itemImage={png3HoursGray}
                         banana={1}
                         onUnlock={() => handleUnlock()}
                         onExchange={() => onExchange('')}
@@ -164,6 +180,45 @@ const ShopContainer = ({
                         }
                         itemImage={pngSpeed}
                         banana={1}
+                        onUnlock={() => handleUnlock()}
+                        onExchange={() => onExchange('')}
+                    />
+                    <ShopItem
+                        itemName={
+                            <>
+                                2 HOURS
+                                <br />
+                                WAIT
+                            </>
+                        }
+                        itemImage={png2HoursGray}
+                        banana={1}
+                        onUnlock={() => handleUnlock()}
+                        onExchange={() => onExchange('')}
+                    />
+                    <ShopItem
+                        itemName={
+                            <>
+                                COMMING
+                                <br />
+                                SOON
+                            </>
+                        }
+                        active={true}
+                        banana={0}
+                        onUnlock={() => handleUnlock()}
+                        onExchange={() => onExchange('')}
+                    />
+                    <ShopItem
+                        itemName={
+                            <>
+                                COMING
+                                <br />
+                                SOON
+                            </>
+                        }
+                        active={true}
+                        banana={0}
                         onUnlock={() => handleUnlock()}
                         onExchange={() => onExchange('')}
                     />
